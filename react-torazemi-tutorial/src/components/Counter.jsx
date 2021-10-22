f61099ef8e2a3add4c2964377c3bf9375a5ff0b2
seitamuro <seitamuro0210@gmail.com>
2021-10-22 17:56:11 +0900

    ライフサイクルの説明を追加

--- react-torazemi-tutorial/src/components/Counter.jsx
+++ react-torazemi-tutorial/src/components/Counter.jsx
@@ -1,7 +1,8 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [count2, setCount2] = useState(0)
 
     const countUp = () => {
         setCount(prevState => prevState + 1)
@@ -11,12 +12,29 @@ const Counter = () => {
         setCount(prevState => prevState - 1)
     }
 
+    const count2Up = () => {
+        setCount2(prevState => prevState + 1)
+    }
+
+    const count2Down = () => {
+        setCount2(prevState => prevState - 1)
+    }
+
+    useEffect( () => {
+        console.log(`counter is ${count}`)
+    }, [count])
+
     return (
         <div>
             <p>count: {count}</p>
 
             <button onClick={countUp}>up</button>
             <button onClick={countDown}>down</button>
+
+            <p>count2: {count2}</p>
+
+            <button onClick={count2Up}>up</button>
+            <button onClick={count2Down}>down</button>
         </div>
     )
 }
