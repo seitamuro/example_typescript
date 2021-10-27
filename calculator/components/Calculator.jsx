fa19b9689e3d90d10598bd5b7aa7b06e7362ddff
seitamuro <seitamuro0210@gmail.com>
2021-10-28 00:49:45 +0900

    add +

--- calculator/components/Calculator.jsx
+++ calculator/components/Calculator.jsx
@@ -1,28 +1,67 @@
 import AnswerDisplay from "./AnswerDisplay"
 import CalculatorButton from "./CalculatorButton"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Calculator() {
     const [answer, setAnswer] = useState("0")
+    const [display, setDisplay] = useState("0")
+    const [stack, setStack] = useState("0")
+    const [operator, setOperator] = useState("")
 
-    const appendValue = (value) => () => 
+    const appendValue = (value) => () => {
         setAnswer(prevState => {
             if (parseInt(prevState) == 0) {
                 return parseInt(value).toString()
             } else {
                 return prevState + value
             }
+        })
+    }
+
+    const deleteValue = () => {
+        setAnswer(prevState => prevState.length <= 1 ? "0" : prevState.slice(0, -1))
+
+        if (answer.length <= 1) {
+            setDisplay("0")
         }
-    )
+    }
 
-    const deleteValue = () => setAnswer(prevState => prevState.length <= 1 ? "0" : prevState.slice(0, -1))
+    const deleteAll = () => {
+        setAnswer("0")
+        setStack("0")
+        setDisplay("0")
+    }
 
-    const deleteAll = () => setAnswer("0")
+    const add = () => {
+        const result = parseInt(stack) + parseInt(answer)
+        setStack(result)
+        setAnswer("0")
+        setDisplay(result)
+        setOperator("+")
+        console.log(`${answer} ${display} ${stack} ${result}`)
+    }
+
+    const equal = () => {
+        switch (operator) {
+            case "+":
+                add()
+                break
+            
+            default:
+                break
+        }
+    }
+
+    useEffect(() => {
+        if (answer != "0") {
+            setDisplay(answer)
+        }
+    }, [answer])
 
     return (
         <div className="container calculator-frame py-4">
-            <AnswerDisplay value={answer} />
+            <AnswerDisplay value={display} />
 
             <div className="row pt-2 px-2">
                 <CalculatorButton className="col m-2 calculator-button-function">税込</CalculatorButton>
@@ -35,25 +74,25 @@ export default function Calculator() {
                 <CalculatorButton className="col m-2" onClick={appendValue("7")}>7</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("8")}>8</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("9")}>9</CalculatorButton>
-                <CalculatorButton className="col m-2">/</CalculatorButton>
+                <CalculatorButton className="col m-2 bg-primary">/</CalculatorButton>
             </div>
             <div className="row p-1 px-2">
                 <CalculatorButton className="col m-2" onClick={appendValue("4")}>4</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("5")}>5</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("6")}>6</CalculatorButton>
-                <CalculatorButton className="col m-2">*</CalculatorButton>
+                <CalculatorButton className="col m-2 bg-primary">*</CalculatorButton>
             </div>
             <div className="row p-1 px-2">
                 <CalculatorButton className="col m-2" onClick={appendValue("1")}>1</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("2")}>2</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("3")}>3</CalculatorButton>
-                <CalculatorButton className="col m-2">-</CalculatorButton>
+                <CalculatorButton className="col m-2 bg-primary">-</CalculatorButton>
             </div>
             <div className="row p-1 px-2">
                 <CalculatorButton className="col m-2" onClick={appendValue("00")}>00</CalculatorButton>
                 <CalculatorButton className="col m-2" onClick={appendValue("0")}>0</CalculatorButton>
-                <CalculatorButton className="col m-2">=</CalculatorButton>
-                <CalculatorButton className="col m-2">+</CalculatorButton>
+                <CalculatorButton className="col m-2 bg-primary" onClick={equal}>=</CalculatorButton>
+                <CalculatorButton className="col m-2 bg-primary" onClick={add}>+</CalculatorButton>
             </div>
         </div>
     )
