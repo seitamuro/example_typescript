86235b470ac5d8717eaaccc977b1ddf24c7ce4bc
seitamuro <seitamuro0210@gmail.com>
2021-10-30 16:58:55 +0900

    add superagent-demo-copy

---
+++ study-postgresql/next-app/components/UserBox.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import request from "superagent"
+
+export default function UserBox() {
+    const [userData, setUserData] = useState([])
+
+    const handleAddUser = (name, mail) => {
+        var url = "/post_user"
+
+        request
+            .post(url)
+            .send({name: name, mail: mail})
+            .end((err, res) => {
+                if (err) {
+                    alert(res.text)
+                }
+
+                var map = JSON.parse(res.text)
+                setUserData(map)
+            })
+    }
+
+    const componentDidMount = () => {}
+
+    return (
+        <div style="width: 300px">
+            <UserForm addUser={handleAddUser} />
+            <hr />
+            <UserList userData={userData} />
+        </div>
+    )
+}
