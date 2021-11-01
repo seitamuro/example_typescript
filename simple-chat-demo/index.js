8ef490ed71b55eaf8a560f55155fea3b5a503651
seitamuro <seitamuro0210@gmail.com>
2021-11-01 23:18:51 +0900

    server

--- simple-chat-demo/index.js
+++ simple-chat-demo/index.js
@@ -1,14 +1,30 @@
-var express = require("express")
-var app = express()
-var http = require("http").Server(app)
-var io = require("socket.io")(http)
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-var port = process.env.PORT || 3000
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
 
-app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/chat.html`)
-})
 
-http.listen(port, () => {
-    console.log(`listening on *: ${port}`)
-})
+var messages = [];
+var users = [];
+
+io.on('connection', function(socket){
+
+  // サーバー側のメッセージリストをクライアント側に送る（emit）
+  socket.emit('init-chat', messages);
+  // クライアント側から送られたメッセージを受け取り、全クライアントに送る（emit）
+  socket.on('c2s-msg', function(msg) {
+      var newMessage = msg;
+      messages.push(newMessage);
+      io.emit('s2c-msg', newMessage);
+  });
+
+});
+
+// httpサーバーを立てる
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
