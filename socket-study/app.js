cbdc52579cf301f5111a33900c72d8f46fae54fd
seitamuro <seitamuro0210@gmail.com>
2021-11-03 19:18:00 +0900

    add Room

--- socket-study/app.js
+++ socket-study/app.js
@@ -6,32 +6,10 @@ app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
 })
 
-app.get("/namespace", (req, res) => {
-    res.sendFile(`${__dirname}/namespace.html`)
-})
-
-var nsp = io.of("/my-namespace")
-var clients = 0
-nsp.on("connection", (socket) => {
-    clients++
-    nsp.emit("hi", `Hello everyone! x ${clients}`)
-
-    socket.on("disconnect", () => {
-        clients--;
-        nsp.emit("hi", `Hello everyone! x ${clients}`)
-    })
-})
-
-var nsp2 = io.of("/my-namespace2")
-var clients2 = 0
-nsp2.on("connection", (socket) => {
-    clients2++
-    nsp2.emit("hi", `Hello everyone! x ${clients2}`)
-
-    socket.on("disconnect", () => {
-        clients2--;
-        nsp.emit("hi", `Hello everyone! x ${clients2}`)
-    })
+var roomno = 1
+io.on("connection", socket => {
+    socket.join(`room-${roomno}`)
+    io.sockets.in(`room-${roomno}`).emit("connectToRoom", `You are in room-${roomno}`)
 })
 
 http.listen(3000, () => {
