2bcae9d25a5585a8b669504ef99e54b7aed2d1a3
seitamuro <seitamuro0210@gmail.com>
2021-11-20 17:55:31 +0900

    typescript express

---
+++ ts-express/README.md
@@ -0,0 +1,14 @@
+# セットアップ
+
+```
+npm init -y
+
+npm install -D typescript
+npm install -D @types/node
+npm install -D ts-node
+
+npx tsc --init
+
+npm install express
+npm install -D @types/express
+```
---
+++ ts-express/index.ts
@@ -0,0 +1,32 @@
+import express from "express"
+
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+})
+
+app.listen(3000, () => {
+    console.log("Start on port 3000.")
+})
+
+type User = {
+    id: number
+    name: string
+    email: string
+}
+
+const users: User[] = [
+    { id: 1, name: "User1", email: "user1@test.local" },
+    { id: 2, name: "User1", email: "user1@test.local" },
+    { id: 3, name: "User1", email: "user1@test.local" },
+]
+
+app.get("/user", (req: express.Request, res: express.Response) => {
+    res.send(JSON.stringify(users))
+})
