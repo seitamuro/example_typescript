d17a07d047aff426c995a7d889ad91e5508bbabf
seitamuro <seitamuro0210@gmail.com>
2021-11-22 16:23:55 +0900

    testing express server

---
+++ testing-express/__tests__/server.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest")
+const app = require("../server/app")
+
+describe("Testing Express Server", () => {
+    test("It should response the GET method", done => {
+        request(app)
+            .get("/")
+            .then(response => {
+                expect(response.statusCode).toBe(200)
+                done() // notify that it ends
+            })
+    })
+})
