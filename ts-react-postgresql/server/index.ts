e037a734e20697110e623a1c0bd8eb9a852d0664
seitamuro <seitamuro0210@gmail.com>
2021-11-22 14:31:33 +0900

    add soket

--- ts-react-postgresql/server/index.ts
+++ ts-react-postgresql/server/index.ts
@@ -1,10 +1,12 @@
+// Importing
 import { Client } from "pg"
 import express from "express"
 import cors from "cors"
 import http from "http"
 import bodyParser from "body-parser"
-import { connect } from "http2"
+import * as socketio from "socket.io"
 
+// create express instance
 const app = express()
 const server = http.createServer(app)
 
@@ -20,6 +22,7 @@ app.use(cors({
     }
 }))
 
+// connect to postgresql
 const client = new Client({
     user: "root",
     host: "127.0.0.1",
@@ -30,6 +33,7 @@ const client = new Client({
 
 client.connect()
 
+// routing
 app.get("/users", (req, res) => {
     client
         .query("SELECT * FROM users")
@@ -37,6 +41,26 @@ app.get("/users", (req, res) => {
         .catch(e => res.send(e.stack))
 })
 
+app.post("/newuser", (req, res) => {
+    const username = req.body.username
+    const email = req.body.email
+
+    console.log(`${username} ${email}`)
+
+    client
+        .query(
+            "INSERT INTO users(username, email) VALUES ($1, $2)",
+            [username, email])
+        .then(_ => res.send("success"))
+        .catch(e => res.send(e.stack))
+})
+
+// start listening
 server.listen(3001, "localhost", () => {
     console.log("Listening on http://localhost:3001")
+})
+
+// socket
+const io: socketio.Server = require("socket.io")(server)
+io.on("connection", (socket: socketio.Socket) => {
 })
--- ts-react-postgresql/src/App.tsx
+++ ts-react-postgresql/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 import {
+  Input,
+  Text,
   Center,
   Button,
   HStack,
@@ -15,12 +17,56 @@ import {
   Td,
   TableCaption,
 } from "@chakra-ui/react"
+import axios  from "axios"
+import { DeleteIcon, AddIcon } from "@chakra-ui/icons"
+
+type User = {
+  user_id: number,
+  username: string,
+  email: string
+}
+
+const getUsers = (callback: (data: (prevState: User[]) => User[]) => void) => {
+  axios.get("http://localhost:3001/users")
+    .then(res => {
+      callback(res.data)
+    })
+    .catch(err => {
+      callback([] as any)
+    })
+}
+
+const postUser = (username: string, email: string) => {
+  const data = {
+    "username": username,
+    "email": email
+  }
+  axios.post("http://localhost:3001/newuser", data)
+    .then(res => console.log(`${res}`))
+    .catch(e => console.log(e.stack))
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([])
+  const [username, setUsername] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
 
   const Increment = () => setCount(prevState => prevState + 1)
   const Decrement = () => setCount(prevState => prevState - 1)
+
+  const deleteUser = (user_id: number) => { console.log(`delete ${user_id}`) }
+
+  const changeUsername = (newname: string) => {
+    setUsername(newname)
+  }
+  const changeEmail = (newemail: string) => {
+    setEmail(newemail)
+  }
+
+  useEffect(() => {
+    getUsers(setUsers)
+  }, [setUsers])
 
   return (
     <>
@@ -33,7 +79,7 @@ function App() {
     </Center>
 
     <Center color="gray.100" borderRadius="10px" border="1px" p="10px" m="10px">
-      <Table variant="simple">
+      <Table variant="simple" size="sm">
         <Thead>
           <Tr>
             <Th>Username</Th>
@@ -41,9 +87,19 @@ function App() {
           </Tr>
         </Thead>
         <Tbody color="black">
-          <Tr>
-            <Td>inches</Td>
-            <Td>millimetres (mm)</Td>
+          {users.map((user, key) => {
+            return (
+              <Tr key={key}>
+                <Td>{user.username}</Td>
+                <Td>{user.email}</Td>
+                <Td><DeleteIcon border="1px" p="2px" w="25px" h="25px" color="red.500" borderRadius="4px" onClick={() => deleteUser(key)} /></Td>
+              </Tr>
+            )
+          })}
+          <Tr key="-1">
+            <Td><Input placeholder="Username" onChange={e => changeUsername(e.target.value)} /></Td>
+            <Td><Input placeholder="Email" onChange={e => changeEmail(e.target.value)} /></Td>
+            <Td><AddIcon color="green.600" w="25px" h="25px" p="2px" onClick={() => postUser(username, email)} /></Td>
           </Tr>
         </Tbody>
       </Table>
