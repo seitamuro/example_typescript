5983ce73e55bae370d663791184639970860c19a
seitamuro <seitamuro0210@gmail.com>
2021-11-23 09:38:58 +0900

    initialize server

--- react-app-with-postgresql/server/handler.js
+++
@@ -1,7 +0,0 @@
-const setupHandler = (io) => {
-    io.on("connection", (socket) => {
-        socket.on("db-updated")
-    })
-}
-
-module.exports = setupHandler
--- react-app-with-postgresql/server/server.js
+++ react-app-with-postgresql/server/server.js
@@ -1,14 +1,27 @@
-const setupHandler = require("./handler")
-const app = require("express")()
-const http = require("http").createServer(app)
-const io = require("socket.io")(http, {
-    cors: {
-        origin: "http://localhost*"
-    }
-})
+// Import
+const pg = require("pg")
+const express = require("express")
+const http = require("http")
 
-http.listen(3001, "localhost", () => {
-    console.log("Listening on http://localhost:3001")
-})
+// create express instance
+const app = express()
+const server = http.createServer(app)
 
-setupHandler(io)
+// setup socket
+const io = require(io)(http)
+
+// connect to PostgreSQL
+const { Client } = require("pg")
+const client = new Client({
+    user: "root",
+    host: "127.0.0.1",
+    database: "postgres",
+    password: "reactappwithpostgresql",
+    port: 5432
+})
+client.connect()
+
+// listening
+server.listen(3001, "localhost", () => {
+    console.log("listening on port 3001")
+})
