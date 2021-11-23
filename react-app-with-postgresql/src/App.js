a91f2891e76eec2d7f1b6e106524d2e2234b1087
seitamuro <seitamuro0210@gmail.com>
2021-11-23 23:34:44 +0900

    add user information table

--- react-app-with-postgresql/src/App.js
+++ react-app-with-postgresql/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Flex,
+  Center,
+} from "@chakra-ui/react"
 
 function App() {
+  const [users, setUsers] = useState([
+    {
+      username: "user1",
+      age: 10,
+      email: "user1@mail.com"
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center>
+      <Table variant="simple" w="60%" border="1px" borderColor="gray.100" borderRadius="md">
+        <Thead>
+          <Tr>
+            <Th>ユーザー名</Th>
+            <Th>年齢</Th>
+            <Th>メールアドレス</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {users.map((i, key) => (
+            <Tr>
+              <Td>{i.username}</Td>
+              <Td>{i.age}</Td>
+              <Td>{i.email}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Center>
   );
 }
 
